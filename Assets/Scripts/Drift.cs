c67aa6163672a012a5140058b0773f1ec708f0bb
nomes <noahbweiner@gmail.com>
2021-02-15 09:33:26 -0800

    add dust particles

---
+++ Assets/Scripts/Drift.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drift : MonoBehaviour
+{
+    public float driftSpeed = 2f;
+
+    private Rigidbody2D rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        var direction = new Vector2(Random.value - .5f, Random.value - .5f).normalized;
+        rigidbody.velocity += direction * Time.deltaTime * driftSpeed;
+    }
+}
