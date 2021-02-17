315e03978eba3fe5cb7382e86df1369a8de000da
nomes <noahbweiner@gmail.com>
2021-02-17 13:26:09 -0800

    add mineable asteroid

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Asteroid : MonoBehaviour
+{
+    public float ore = 1000f;
+    public float scaleRatio = 0.01f;
+
+    void Start() { }
+
+    void Update()
+    {
+        var volume = ore * scaleRatio;
+        var radius = Mathf.Pow(3f / (4f * Mathf.PI) * volume, 1f / 3f);
+        transform.localScale = Vector3.one * radius;
+    }
+}
--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -7,6 +7,8 @@ public class ShipController : MonoBehaviour
     public float throttle = 0;
     public float moveSpeed;
     public Vector3? target;
+    public float miningDistance;
+    public float miningSpeed;
 
     public Vector3 Heading => transform.up;
     public Vector3 Position => transform.position;
@@ -22,5 +24,24 @@ public class ShipController : MonoBehaviour
     void FixedUpdate()
     {
         rigidbody.velocity += transform.up * throttle * moveSpeed * Time.deltaTime;
+    }
+
+    void Update()
+    {
+        var asteroids = GameObject.FindObjectsOfType<Asteroid>();
+
+        foreach (var asteroid in asteroids)
+        {
+            var distance = (asteroid.transform.position - transform.position).magnitude;
+            if (distance <= miningDistance)
+            {
+                asteroid.ore -= miningSpeed * Time.deltaTime;
+
+                if (asteroid.ore <= 0f)
+                {
+                    GameObject.Destroy(asteroid.gameObject);
+                }
+            }
+        }
     }
 }
