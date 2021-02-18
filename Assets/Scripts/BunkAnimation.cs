748ce106b0c0a29ef513fc463f3f8bef10f0bab2
nomes <noahbweiner@gmail.com>
2021-02-18 11:01:59 -0800

    add interactible bunks

---
+++ Assets/Scripts/BunkAnimation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class BunkAnimation : MonoBehaviour
+{
+    public Sprite[] openFrames;
+    public Sprite[] closedFrames;
+
+    public bool isOpen = true;
+    private bool wasOpen = true;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (isOpen && !wasOpen)
+        {
+            spriteRenderer.sprite = openFrames[Random.Range(0, openFrames.Length)];
+            wasOpen = isOpen;
+        }
+        else if (!isOpen && wasOpen)
+        {
+            spriteRenderer.sprite = closedFrames[Random.Range(0, openFrames.Length)];
+            wasOpen = isOpen;
+        }
+    }
+}
---
+++ Assets/Scripts/BunksController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunksController : MonoBehaviour, IInteractible
+{
+    public PlayerController topBunkOwner;
+    public PlayerController bottomBunkOwner;
+
+    public BunkAnimation topBunk;
+    public BunkAnimation bottomBunk;
+
+    private bool isTopBunkOccupied = false;
+    private bool isBottomBunkOccupied = false;
+
+    public bool CanInteract() => true;
+    public string GetActionText(PlayerController player) => "rest";
+
+    public void Interact(PlayerController player)
+    {
+        if (player == topBunkOwner)
+        {
+            isTopBunkOccupied = true;
+        }
+
+        if (player == bottomBunkOwner)
+        {
+            isBottomBunkOccupied = true;
+        }
+
+        player.instructionText.text = $"{Icons.IconText(player.input.inputScheme.btn2)} cancel";
+        player.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        topBunk.isOpen = !isTopBunkOccupied;
+        bottomBunk.isOpen = !isBottomBunkOccupied;
+
+        if (isTopBunkOccupied && topBunkOwner.input.GetBtnDown(2))
+        {
+            isTopBunkOccupied = false;
+            topBunkOwner.gameObject.SetActive(true);
+        }
+
+        if (isBottomBunkOccupied && bottomBunkOwner.input.GetBtnDown(2))
+        {
+            isBottomBunkOccupied = false;
+            bottomBunkOwner.gameObject.SetActive(true);
+        }
+    }
+}
