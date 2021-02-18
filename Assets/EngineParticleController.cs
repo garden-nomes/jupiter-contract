04b11e8a1bc8b657d44bd55a1e44082b1c51c8ff
nomes <noahbweiner@gmail.com>
2021-02-18 01:42:22 -0800

    add engine particles

---
+++ Assets/EngineParticleController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngineParticleController : MonoBehaviour
+{
+    public EngineController engine;
+    public float maxParticleSpeed;
+    public float minParticleSpeed;
+
+    private new ParticleSystem particleSystem;
+
+    void Start()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        var emission = particleSystem.emission;
+        if (engine.IsBroken || engine.throttle == 0f)
+        {
+            emission.enabled = false;
+        }
+        else
+        {
+            emission.enabled = true;
+
+            var main = particleSystem.main;
+            main.startSpeed = Mathf.Max(engine.throttle * maxParticleSpeed, minParticleSpeed);
+        }
+    }
+}
