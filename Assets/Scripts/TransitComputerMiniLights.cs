25fa154d482ca47031c03c1092963c3902a5ea87
nomes <noahbweiner@gmail.com>
2021-02-19 13:26:34 -0800

    add little transit lights to main ship view

---
+++ Assets/Scripts/TransitComputerMiniLights.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitComputerMiniLights : MonoBehaviour
+{
+    public TransitComputer transitComputer;
+
+    public GameObject distAlignLight;
+    public GameObject speedAccLight;
+    public GameObject stage0Light;
+    public GameObject stage1Light;
+
+    void LateUpdate()
+    {
+        distAlignLight.SetActive(
+            transitComputer.distText.text != "" || transitComputer.alignText.text != "");
+        speedAccLight.SetActive(
+            transitComputer.speedText.text != "" || transitComputer.accText.text != "");
+        stage0Light.SetActive(transitComputer.stage0Light.gameObject.activeSelf);
+        stage1Light.SetActive(transitComputer.stage1Light.gameObject.activeSelf);
+    }
+}
