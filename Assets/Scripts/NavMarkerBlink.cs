00e25a47deddcdf4d02e5d4dddb71321359684dd
nomes <noahbweiner@gmail.com>
2021-02-19 13:06:08 -0800

    blink navigational markers to stand out against light bg

---
+++ Assets/Scripts/NavMarkerBlink.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NavMarkerBlink : MonoBehaviour
+{
+    [Min(0f)] public float rate = 1f;
+    public Color color1 = Color.white;
+    public Color color2 = Color.black;
+
+    private Image image;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        image.color = (Time.time % rate < rate * 0.5f) ? color1 : color2;
+    }
+}
