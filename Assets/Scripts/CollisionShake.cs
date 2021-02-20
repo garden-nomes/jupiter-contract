9b2cecd708ec2d430596865b9071e3eb9ca6b7fe
nomes <noahbweiner@gmail.com>
2021-02-19 22:36:56 -0800

    add screen shake on collision

---
+++ Assets/Scripts/CollisionShake.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionShake : MonoBehaviour
+{
+    public Shake shake;
+
+    public float time = 1f;
+    public float amplitude = 100f;
+
+    private float collisionTimer = 0f;
+
+    private void Update()
+    {
+
+        if (collisionTimer > 0f)
+        {
+            shake.Add((collisionTimer / time) * amplitude);
+            collisionTimer -= Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        collisionTimer = time;
+    }
+
+    [ContextMenu("Test")]
+    private void Test()
+    {
+        collisionTimer = time;
+    }
+}
