f310760b6dd9f30541fc82584722c14de85e4cba
nomes <noahbweiner@gmail.com>
2021-02-20 20:46:58 -0800

    shrink galley

---
+++ Assets/Scripts/BoxEdit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class BoxEdit : MonoBehaviour
+{
+    public Vector2 a;
+    public Vector2 b;
+    public float pixelsPerUnit = 8f;
+
+    void Awake()
+    {
+        var extent = new Vector2(
+            (transform.localScale.x - 1f / 8f) / 2f,
+            (transform.localScale.y - 1f / 8f) / 2f
+        );
+
+        a = (Vector2) transform.position - extent;
+        b = (Vector2) transform.position + extent;
+    }
+
+    public void Move()
+    {
+        var height = Mathf.Abs(a.y - b.y) + 1 / 8f;
+        var width = Mathf.Abs(a.x - b.x) + 1 / 8f;
+
+        transform.position = new Vector3(0f, 0f, transform.position.z) + (Vector3) (a + b) / 2f;
+        transform.localScale = new Vector3(width, height, 1f);
+    }
+}
--- Assets/Scripts/Editor/BoxEditEditor.cs
+++ Assets/Scripts/Editor/BoxEditEditor.cs
@@ -1,5 +1,3 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
@@ -33,7 +31,7 @@ public class BoxEditEditor : Editor
             Undo.RecordObject(wall, "Reposition wall");
             wall.a = snapToPixelGrid(newPositionA);
             wall.b = snapToPixelGrid(newPositionB);
-            wall.Update();
+            wall.Move();
         }
     }
 
@@ -47,33 +45,5 @@ public class BoxEditEditor : Editor
             Mathf.Floor(v.y * pixelsPerUnit) / pixelsPerUnit,
             Mathf.Floor(v.z * pixelsPerUnit) / pixelsPerUnit
         );
-    }
-}
-
-[ExecuteInEditMode]
-public class BoxEdit : MonoBehaviour
-{
-    [HideInInspector] public Vector2 a;
-    [HideInInspector] public Vector2 b;
-    public float pixelsPerUnit = 8f;
-
-    void Awake()
-    {
-        var extent = new Vector2(
-            (transform.localScale.x - 1f / 8f) / 2f,
-            (transform.localScale.y - 1f / 8f) / 2f
-        );
-
-        a = (Vector2) transform.position - extent;
-        b = (Vector2) transform.position + extent;
-    }
-
-    public virtual void Update()
-    {
-        var height = Mathf.Abs(a.y - b.y) + 1 / 8f;
-        var width = Mathf.Abs(a.x - b.x) + 1 / 8f;
-
-        transform.position = new Vector3(0f, 0f, transform.position.z) + (Vector3) (a + b) / 2f;
-        transform.localScale = new Vector3(width, height, 1f);
     }
 }
